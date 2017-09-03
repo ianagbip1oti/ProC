34837600f5cffa3065d5e3328680d6257d4df92b
Lana <ianagbip1oti@gmail.com>
2017-09-03 02:42:26 +0000

    Make StringLiteral an Expr

--- src/ProC/Language.hs
+++ src/ProC/Language.hs
@@ -1,14 +1,25 @@
+{-# LANGUAGE GADTs #-}
 module ProC.Language where
 
 import Control.Monad
 
+data Expression a where
+    StringLiteral :: String -> Expression String
+
+instance (Show a) => Show (Expression a) where
+  show (StringLiteral s) = show s
+
+
+eval :: Expression a -> a
+eval (StringLiteral s) = s
+
 data Statement =
     Noop
-    | Print String
+    | Print (Expression String)
     | Seq [Statement]
     deriving (Show)
     
 exec :: Statement -> IO ()
 exec (Noop) = return ()
-exec (Print s) = putStrLn s
+exec (Print s) = putStrLn $ eval s
 exec (Seq ss) = forM_ ss exec
