f3b7ff293ab8a619d07dd2fb544304563fda9950
Lana <ianagbip1oti@gmail.com>
2017-09-03 23:04:57 +0000

    Allow concatenation of String and Numeric
    Makes NumericExpression a StringExpression, which makes it usable in StringExpression

--- src/ProC/Language.hs
+++ src/ProC/Language.hs
@@ -8,6 +8,15 @@ module ProC.Language where
 
 import Control.Monad
 
+class ToString s where
+    toString :: s -> String
+    
+instance ToString String where
+    toString = id
+    
+instance ToString Integer where
+    toString = show
+
 class Eval exp res | exp -> res where
   eval :: exp -> res
 
@@ -16,19 +25,22 @@ data NumericExpression = IntLiteral Integer
 instance Eval NumericExpression Integer where
     eval (IntLiteral i) = i
 
-data StringExpression =
-  StringLiteral String | StringConcat StringExpression StringExpression
+data StringExpression where
+  StringFromNumericExpression :: NumericExpression -> StringExpression
+  StringLiteral :: String -> StringExpression
+  StringConcat :: StringExpression -> StringExpression -> StringExpression
 
 instance Eval StringExpression String where
     eval (StringLiteral s) = s
-    eval (StringConcat l r) = eval l ++ eval r
+    eval (StringConcat l r) = mconcat $ toString . eval <$> [l, r]
+    eval (StringFromNumericExpression n) = toString $ eval n
 
 data Statement where
     Noop :: Statement
-    Print :: (Eval e s, Show s) => e -> Statement
+    Print :: StringExpression -> Statement
     Seq :: [Statement] -> Statement
         
 exec :: Statement -> IO ()
 exec (Noop) = return ()
-exec (Print s) = putStrLn . show $ eval s
+exec (Print s) = putStrLn $ eval s
 exec (Seq ss) = forM_ ss exec
