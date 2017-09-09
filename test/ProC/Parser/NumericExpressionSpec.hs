a3e75da58947e95b6543eae63efd337fd61b9e2c
Lana <ianagbip1oti@gmail.com>
2017-09-09 03:13:47 +0000

    Add more specs for numeric expressions

--- test/ProC/Parser/NumericExpressionSpec.hs
+++ test/ProC/Parser/NumericExpressionSpec.hs
@@ -9,6 +9,18 @@ import           ProC.Parser.ProC
 import           Test.Hspec
 import           Test.QuickCheck
 
+instance Arbitrary NumericBinOp where
+  arbitrary = elements [Add, Subtract, Multiply, Divide]
+
+formatBinOp :: NumericBinOp -> String
+formatBinOp Add      = "+"
+formatBinOp Subtract = "-"
+formatBinOp Multiply = "*"
+formatBinOp Divide   = "/"
+
+one :: NumericExpression
+one = IntLiteral 1
+
 spec :: Spec
 spec =
   describe "term" $ do
@@ -17,3 +29,6 @@ spec =
     it "should parse negative int literals" $ property $ \i ->
       i < 0 ==> parse numericExpression (show i) ==
       Right (UnaryOp Negate (IntLiteral (-i)))
+    it "should parse binary ops" $ property $ \op ->
+      parse numericExpression ("1 " ++ formatBinOp op ++ " 1") `shouldBe`
+      Right (BinOp op one one)
