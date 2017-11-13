a8a6c3b30bbf4e74ea369054a0b0115adb9a141a
Lana <ianagbip1oti@gmail.com>
2017-11-13 03:01:27 +0000

    Formatting

--- src/ProC/Interpreter.hs
+++ src/ProC/Interpreter.hs
@@ -64,11 +64,11 @@ exec :: Statement -> ContextM ()
 exec (BlnVarDcl n e) = eval e >>= setVarM n
 exec (IntVarDcl n e) = eval e >>= setVarM n
 exec (StrVarDcl n e) = eval e >>= setVarM n
-exec Noop             = return ()
-exec (Print s)        = eval s >>= liftIO . putStrLn
-exec (Seq ss)         = forM_ ss exec
-exec (Block ss)       = enterBlockM >> forM_ ss exec >> exitBlockM
-exec (Whl e ss)       = eval e >>= (flip when . exec) (Block ss)
+exec Noop            = return ()
+exec (Print s)       = eval s >>= liftIO . putStrLn
+exec (Seq ss)        = forM_ ss exec
+exec (Block ss)      = enterBlockM >> forM_ ss exec >> exitBlockM
+exec (Whl e ss)      = eval e >>= (flip when . exec) (Block ss)
 
 runProC :: ProCProgram -> IO ()
 runProC p = evalContextM (exec p)
